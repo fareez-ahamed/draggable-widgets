323fec725e93b7dff403d079c73a9f96264f2047
Fareez Ahamed <fareez.ahamed@gmail.com>
2022-06-17 21:13:54 +0530

    Enhanced version of the dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -19,15 +19,47 @@ function Widget({ content, onDragStart }) {
       style={{
         backgroundColor: "#eeeeee",
         borderRadius: "5px",
-        minHeight: "5rem",
+        minHeight: "10rem",
+        padding: "1rem 1rem",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        flexDirection: "column",
       }}
-      onDragStart={onDragStart}
-      draggable
     >
-      {content}
+      <div style={{ display: "flex", paddingBottom: "0.5rem" }}>
+        <p style={{ flex: "1", margin: "0rem" }}>Title {content}</p>
+        <div onDragStart={onDragStart} draggable>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "2rem",
+          flex: "1",
+          borderRadius: "5px",
+          fontSize: "4rem",
+          color: "#cccccc",
+        }}
+      >
+        {content}
+      </div>
     </div>
   );
 }
